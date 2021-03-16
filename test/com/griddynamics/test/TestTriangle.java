b607e0a861f70b425e354797b53ea0283b9efc90
mkolegova <kolegovamasha@mail.ru>
2021-03-16 15:32:21 +0300

    Issue 01. HW 01. Triangle Analysis System Improved.

--- test/com/griddynamics/test/TestTriangle.java
+++ test/com/griddynamics/test/TestTriangle.java
@@ -1,10 +1,10 @@
-package triangle.analysis.task;
+package com.griddynamics.test;
 
-import org.junit.Test;
-import triangle.analysis.task.Triangle.TriangleType;
+import com.griddynamics.main.Triangle.TriangleType;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertSame;
-import static triangle.analysis.task.Triangle.triangleAnalysisSystem;
+import static com.griddynamics.main.Triangle.triangleAnalysisSystem;
+import static org.testng.Assert.assertSame;
 
 public class TestTriangle {
     @Test
@@ -38,6 +38,18 @@ public class TestTriangle {
     }
 
     @Test
+    public void testRecEqual1() {
+        TriangleType triangleType = triangleAnalysisSystem(Math.sqrt(2), Math.sqrt(2), 2);
+        assertSame(triangleType, TriangleType.RECTANGULAR_ISOSCELES_TRIANGLE);
+    }
+
+    @Test
+    public void testRecEqual2() {
+        TriangleType triangleType = triangleAnalysisSystem(12.5, 12.5, 12.5 * Math.sqrt(2));
+        assertSame(triangleType, TriangleType.RECTANGULAR_ISOSCELES_TRIANGLE);
+    }
+
+    @Test
     public void testTwoEqual1() {
         TriangleType triangleType = triangleAnalysisSystem(31, 31, 26);
         assertSame(triangleType, TriangleType.ISOSCELES_TRIANGLE);
@@ -53,6 +65,24 @@ public class TestTriangle {
     public void testTwoEqual3() {
         TriangleType triangleType = triangleAnalysisSystem(43.31844, 43.31844, 28);
         assertSame(triangleType, TriangleType.ISOSCELES_TRIANGLE);
+    }
+
+    @Test
+    public void testRec1() {
+        TriangleType triangleType = triangleAnalysisSystem(13, 18, Math.sqrt(493));
+        assertSame(triangleType, TriangleType.RECTANGULAR_TRIANGLE);
+    }
+
+    @Test
+    public void testRec2() {
+        TriangleType triangleType = triangleAnalysisSystem(5, 3, 4);
+        assertSame(triangleType, TriangleType.RECTANGULAR_TRIANGLE);
+    }
+
+    @Test
+    public void testRec3() {
+        TriangleType triangleType = triangleAnalysisSystem(3.54, Math.sqrt(38.746), 5.12);
+        assertSame(triangleType, TriangleType.RECTANGULAR_TRIANGLE);
     }
 
     @Test
