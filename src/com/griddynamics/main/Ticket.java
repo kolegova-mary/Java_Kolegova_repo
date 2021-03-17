fa2b38b5d1e2842379d935519ab92db1e66f6f84
mkolegova <kolegovamasha@mail.ru>
2021-03-17 19:15:53 +0300

    Issue 00. Lucky Ticket fixed.

--- src/com/griddynamics/main/Ticket.java
+++ src/com/griddynamics/main/Ticket.java
@@ -11,43 +11,31 @@ public class Ticket {
         in.close();
     }
 
-    public static Answer isMyTicketLucky(String ticketNumber) {
+    public static boolean isMyTicketLucky(String ticketNumber) throws IllegalArgumentException {
         if (ticketNumber.matches("\\d{6}")) {
             char[] x = new char[3];
             char[] y = new char[3];
             ticketNumber.getChars(0, 3, x, 0);
             ticketNumber.getChars(3, 6, y, 0);
-            int sumX = 0;
-            int sumY = 0;
-            for (int i = 0; i < 3; i++) {
-                sumX += x[i];
-                sumY += y[i];
-            }
+            int sumX = sum(x);
+            int sumY = sum(y);
             if (sumX == sumY) {
                 System.out.println("Congratulations! You have a lucky ticket!");
-                return Answer.LUCKY;
+                return true;
             } else {
                 System.out.println("Sorry, try again later.");
-                return Answer.NOT_LUCKY;
+                return false;
             }
         } else {
-            System.out.println("Give correct ticket, please.");
-            return Answer.INVALID_TICKET;
+            throw new IllegalArgumentException("Give correct ticket, please.");
         }
     }
 
-    public static int sum(int x) {
+    public static int sum(char[] ticketHalf) {
         int sum = 0;
-        while (x != 0) {
-            sum += x % 10;
-            x /= 10;
+        for (int i = 0; i < 3; i++) {
+            sum += ticketHalf[i];
         }
         return sum;
-    }
-
-    public enum Answer {
-        LUCKY,
-        NOT_LUCKY,
-        INVALID_TICKET
     }
 }
--- test/com/griddynamics/test/TicketTest.java
+++ test/com/griddynamics/test/TicketTest.java
@@ -1,82 +1,72 @@
 package com.griddynamics.test;
 
-import com.griddynamics.main.Ticket.Answer;
+import com.griddynamics.main.Ticket;
 import org.testng.annotations.Test;
 
 import static com.griddynamics.main.Ticket.isMyTicketLucky;
 import static org.testng.Assert.assertSame;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
 
 public class TicketTest {
 
-    @Test
-    public void testNegativeValue() {
-        Answer myTicketLucky = isMyTicketLucky("-1");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testNegativeValue() throws IllegalArgumentException {
+        isMyTicketLucky("-1");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testShortValue() throws IllegalArgumentException{
+       isMyTicketLucky("2882");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testZeroValue() throws IllegalArgumentException{
+        isMyTicketLucky("0");
     }
 
     @Test
-    public void testShortValue() {
-        Answer myTicketLucky = isMyTicketLucky("2882");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
+    public void testRepeatedValue()throws IllegalArgumentException {
+        assertTrue(isMyTicketLucky("101101"));
     }
 
     @Test
-    public void testZeroValue() {
-        Answer myTicketLucky = isMyTicketLucky("0");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
+    public void testSumValue() throws IllegalArgumentException{
+        assertTrue(isMyTicketLucky("346922"));
     }
 
     @Test
-    public void testRepeatedValue() {
-        Answer myTicketLucky = isMyTicketLucky("101101");
-        assertSame(myTicketLucky, Answer.LUCKY);
+    public void testSumValueWithZero() throws IllegalArgumentException{
+        assertTrue(isMyTicketLucky("056902"));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testLongValue() throws IllegalArgumentException{
+        isMyTicketLucky("78567856");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSpecialChars() throws IllegalArgumentException{
+        isMyTicketLucky("0^&1@$");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testEmpty() throws IllegalArgumentException{
+       isMyTicketLucky("");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSpace()throws IllegalArgumentException {
+       isMyTicketLucky(" 029 0");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testExpression()throws IllegalArgumentException {
+       isMyTicketLucky("12.3+1");
     }
 
     @Test
-    public void testSumValue() {
-        Answer myTicketLucky = isMyTicketLucky("346922");
-        assertSame(myTicketLucky, Answer.LUCKY);
-    }
-
-    @Test
-    public void testSumValueWithZero() {
-        Answer myTicketLucky = isMyTicketLucky("056902");
-        assertSame(myTicketLucky, Answer.LUCKY);
-    }
-
-    @Test
-    public void testLongValue() {
-        Answer myTicketLucky = isMyTicketLucky("78567856");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
-    }
-
-    @Test
-    public void testSpecialChars() {
-        Answer myTicketLucky = isMyTicketLucky("0^&1@$");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
-    }
-
-    @Test
-    public void testEmpty() {
-        Answer myTicketLucky = isMyTicketLucky("");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
-    }
-
-    @Test
-    public void testSpace() {
-        Answer myTicketLucky = isMyTicketLucky(" 029 0");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
-    }
-
-    @Test
-    public void testExpression() {
-        Answer myTicketLucky = isMyTicketLucky("12.3+1");
-        assertSame(myTicketLucky, Answer.INVALID_TICKET);
-    }
-
-    @Test
-    public void testWrongValue() {
-        Answer myTicketLucky = isMyTicketLucky("123456");
-        assertSame(myTicketLucky, Answer.NOT_LUCKY);
+    public void testWrongValue()throws IllegalArgumentException {
+        assertFalse(isMyTicketLucky("123456"));
     }
 }
