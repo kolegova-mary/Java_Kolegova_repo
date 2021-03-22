a9de225766577cbc8fb2b5165b5ed917b8cca135
mkolegova <kolegovamasha@mail.ru>
2021-03-22 13:53:53 +0300

    Issue 03. Cars And Trucks To Different Files Fixed.

---
+++ src/test/java/com/griddynamics/testSerializationTask/TestSerializationSolution.java
@@ -0,0 +1,60 @@
+package com.griddynamics.testSerializationTask;
+
+import com.griddynamics.serializationTask.SerializationSolution;
+import org.testng.annotations.Test;
+import com.griddynamics.serializationTask.SerializationSolution.FileFormatException;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.fail;
+
+public class TestSerializationSolution {
+
+    @Test
+    public void testUsual() throws FileFormatException, IOException {
+        String filePath = getFilePath("vehicle_data.txt");
+        SerializationSolution.readFromMainFile(filePath);
+    }
+
+    @Test(expectedExceptions = FileFormatException.class)
+    public void testInvalidValue() throws IOException, FileFormatException {
+        SerializationSolution.readFromMainFile(getFilePath("invalid_value.txt"));
+    }
+
+    @Test(expectedExceptions = IOException.class)
+    public void testNotExistFile() throws IOException, FileFormatException {
+        SerializationSolution.readFromMainFile(getFilePath("not-existed.txt"));
+    }
+
+    @Test(expectedExceptions = FileFormatException.class)
+    public void testMissedComma() throws IOException, FileFormatException {
+        SerializationSolution.readFromMainFile(getFilePath("missed_comma.txt"));
+    }
+
+    @Test
+    public void testRightLineMissedComma() throws IOException {
+        try {
+            SerializationSolution.readFromMainFile(getFilePath("missed_comma.txt"));
+        } catch (FileFormatException e) {
+            assertEquals(e.getLineNumber(), 2);
+        }
+    }
+
+    @Test
+    public void textEmpty() throws IOException, FileFormatException {
+        SerializationSolution.readFromMainFile(getFilePath("empty.txt"));
+    }
+
+    @Test(expectedExceptions = FileFormatException.class)
+    public void testExtraComma() throws IOException, FileFormatException {
+        SerializationSolution.readFromMainFile(getFilePath("extra_comma.txt"));
+    }
+
+    private String getFilePath(String fileName) {
+        URL url = getClass().getClassLoader().getResource(fileName);
+        return url == null ? "" : url.getPath();
+    }
+
+}
